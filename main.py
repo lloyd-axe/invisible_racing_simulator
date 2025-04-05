5cf22cbe28896a75c59a508233615349ead8575f
lloyd-axe <lloydacha@gmail.com>
2025-04-05 13:18:15 +0800

    update 1

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ class HolisticModel:
         frame.flags.writeable = writeable
         return cv2.cvtColor(frame, color)
 
-    def _draw_landmarks(self, frame: Any, results: Any, other_points: list, lines: list) -> Any:
+    def _draw_landmarks(self, frame: Any, results: Any, other_points: list, lines: list,
+                        line_color: tuple = (0, 0, 255)) -> Any:
         if results.pose_landmarks:
             self.drawing_utils.draw_landmarks(
                 frame,
@@ -59,19 +60,21 @@ class HolisticModel:
                 connection_drawing_spec=self.drawing_styles.get_default_hand_connections_style()
             )
         for p in other_points:
-            cv2.circle(frame, p, 5, (0, 0, 255), cv2.FILLED)
+            cv2.circle(frame, p, 5, (255, 255, 255), cv2.FILLED)
         for line in lines:
             if len(line) == 2:
-                cv2.line(frame, line[0], line[1], (0, 0, 255), 2)
+                cv2.line(frame, line[0], line[1], line_color, 2)
         return frame
 
 
 class InvisibleRacingSim(HolisticModel):
     def __init__(self, throttle_key: str, break_key: str, steer_left_key: str, steer_right_key: str,
                  steer_threshold: int, delta_angle_threshold: int, break_threshold: int, throttle_threshold: int,
+                 show_video: bool = True,
                  min_detection_confidence: float = 0.5, min_tracking_confidence: float = 0.5):
         super().__init__(min_detection_confidence, min_tracking_confidence)
         self.frame_shape = None
+        self.show_video = show_video
         self.steer_threshold = steer_threshold
         self.delta_angle_threshold = delta_angle_threshold
         self.break_threshold = break_threshold
@@ -93,10 +96,10 @@ class InvisibleRacingSim(HolisticModel):
             'space': Key.space
         }
 
-    def non_letter_keys(self, key):
+    def non_letter_keys(self, key: str) -> str:
         return self.key_dict.get(key, key)
 
-    def get_hand_points(self, results):
+    def get_hand_points(self, results: Any) -> tuple:
         pose_landmarks = [[res.x, res.y, res.z] for res in results.pose_landmarks.landmark] \
             if results.pose_landmarks else np.zeros((33, 3))
         if np.array(pose_landmarks).any():
@@ -132,7 +135,7 @@ class InvisibleRacingSim(HolisticModel):
         state_direction = 0 if percent_turn < self.steer_threshold else state_direction
         return distance, percent_turn, delta_angle, state_direction
 
-    def __steer(self, percent_turn, delta_angle, state_direction, prev_state_direction):
+    def __steer(self, percent_turn: float, delta_angle: float, state_direction: int, prev_state_direction: int) -> int:
         if percent_turn > self.steer_threshold:
             if state_direction == prev_state_direction:
                 if abs(delta_angle) > self.delta_angle_threshold:
@@ -157,18 +160,23 @@ class InvisibleRacingSim(HolisticModel):
         else:
             self.keyboard_controller.release(self.steer_left_key)
             self.keyboard_controller.release(self.steer_right_key)
+        return state_direction
 
-    def __throttle(self, distance):
+    def __throttle(self, distance: float) -> int:
+        throttle_state = 0
         if distance > self.throttle_threshold:
             self.keyboard_controller.press(self.throttle_key)
             self.keyboard_controller.release(self.break_key)
+            throttle_state = 1
         else:
             if distance < self.break_threshold:
                 self.keyboard_controller.press(self.break_key)
                 self.keyboard_controller.release(self.throttle_key)
+                throttle_state = 2
             else:
                 self.keyboard_controller.release(self.break_key)
                 self.keyboard_controller.release(self.throttle_key)
+        return throttle_state
 
     def run(self):
         prev_perc_turn = 0
@@ -186,21 +194,39 @@ class InvisibleRacingSim(HolisticModel):
             right_mid, left_mid = self.get_hand_points(results)
 
             frame = self._frame_adjustments(frame, cv2.COLOR_RGB2BGR, True)
-            frame = self._draw_landmarks(frame, results, [right_mid, left_mid], [[right_mid, left_mid]])
+            steering_line_color = (0, 0, 0)
+            direction = "STRAIGHT"
             if right_mid and left_mid:
                 distance, percent_turn, delta_angle, state_direction = \
                     self.__calculate_values(right_mid, left_mid, prev_perc_turn)
 
-                self.__steer(percent_turn, delta_angle, state_direction, prev_state_direction)
-                self.__throttle(distance)
+                state_direction = self.__steer(percent_turn, delta_angle, state_direction, prev_state_direction)
+                throttle_state = self.__throttle(distance)
+                if throttle_state == 1:
+                    steering_line_color = (0, 255, 0)
+                elif throttle_state == 2:
+                    steering_line_color = (0, 0, 255)
 
-            prev_perc_turn = percent_turn
-            prev_state_direction = state_direction
+                if state_direction == 1:
+                    direction = "LEFT"
+                elif state_direction == 2:
+                    direction = "RIGHT"
 
-            cv2.imshow('Invisible Racing Sim', cv2.flip(frame, 1))
+                prev_perc_turn = percent_turn
+                prev_state_direction = state_direction
+
+            frame = self._draw_landmarks(frame, results, [right_mid, left_mid], [[right_mid, left_mid]],
+                                         steering_line_color)
+
+            resized_frame = cv2.resize(frame, (330, 300))
+            flipped_frame = cv2.flip(resized_frame, 1)
+            cv2.putText(flipped_frame, direction,
+                        (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2, cv2.LINE_AA
+                        )
+            if self.show_video:
+                cv2.imshow('Invisible Steering Wheel Simulator', flipped_frame)
             if cv2.waitKey(1) & 0xFF == 27:
                 break
-
         self.release_resources()
 
 
@@ -216,9 +242,11 @@ if __name__ == '__main__':
     delta_angle_threshold = int(config["sensitivity"]["delta_angle_threshold"])
     break_threshold = int(config["sensitivity"]["break_threshold"])
     throttle_threshold = int(config["sensitivity"]["throttle_threshold"])
+    show_video = bool(config["video"]["show_video"])
 
     inst = InvisibleRacingSim(throttle_key=throttle_key, break_key=break_key,
                               steer_left_key=steer_left_key, steer_right_key=steer_right_key,
                               steer_threshold=steer_threshold, delta_angle_threshold=delta_angle_threshold,
-                              break_threshold=break_threshold, throttle_threshold=throttle_threshold)
+                              break_threshold=break_threshold, throttle_threshold=throttle_threshold,
+                              show_video=show_video)
     inst.run()
